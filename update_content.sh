af45520e218e39001e2502dd749b9f71fe9cf547
dabreu <mariana.dabreu@servicemax.com>
2021-07-04 22:09:10 +0000

    Changes done by update_content script

--- update_content.sh
+++ update_content.sh
@@ -29,22 +29,18 @@ echo "<pre>"
 
 if [ $diff -eq 0 ] 
 then
-  echo 'No se detectaron cambios, no se actualiz&oacute; el sitio' > $catalog_export_dir/index.html
+  echo 'No se detectaron cambios, no se actualiz&oacute; el sitio'
 else
   # Guardar los cambios en los sources para mostrar en el resultado
-  git diff --color-words -U0 --src-prefix=Ficha: *.md | grep -v @@ | grep -v index | grep -v diff | grep -v -e "+++ b" > $catalog_export_dir/index.html
+  git diff --color-words -U0 --src-prefix=Ficha: *.md | grep -v @@ | grep -v index | grep -v diff | grep -v -e "+++ b"
 
   # Rebuildear y deployar los cambios en el site
-  ./deploy.sh > /tmp/deploy.out 2>&1 
+  ./deploy.sh > /tmp/deploy.out 
 
   # Guardar los cambios en los sources
-  git commit -am "Changes done by update_content script" 2>&1
-  git push origin master 2>&1
+  git commit -am "Changes done by update_content script" > /tmp/commit.out 2>&1
+  git push origin master > /tmp/push.out 2>&1
 fi
-
-cat $catalog_export_dir/index.html
 
 echo "</pre>"
 echo "</html>"
-
-rm $catalog_export_dir/index.html
